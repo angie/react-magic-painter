54488f5e6eaff73cd1f7c29b31ddf506b51efc3d
Angie Merryweather <6738119+angie@users.noreply.github.com>
2021-11-17 12:04:09 +0000

    change some copy

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -8,9 +8,9 @@ interface Props {
 export const Intro: React.FC<Props> = ({init, isReady}) => (
   <header className={isReady ? 'hidden intro' : 'intro'}>
     <div className="intro__content">
-      <h1>Magic Painter</h1>
+      <h1>Drawing Check-Ins</h1>
       <button className="blob-btn" type="button" onClick={init}>
-        <span className="blob-text">Start painting</span>
+        <span className="blob-text">Start drawing</span>
         <span className="blob-btn__inner">
           <span className="blob-btn__blobs">
             <span className="blob-btn__blob"/>
@@ -20,42 +20,6 @@ export const Intro: React.FC<Props> = ({init, isReady}) => (
           </span>
         </span>
       </button>
-      <p>
-        Created by <strong>Adrian Bece</strong>
-      </p>
-      <p>
-        <a
-          href="https://codeadrian.hashnode.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hashnode
-        </a>{' '}
-        |
-        <a
-          href="https://twitter.com/AdrianBeceDev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>{' '}
-        |
-        <a
-          href="https://www.buymeacoffee.com/ubnZ8GgDJ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Support my work
-        </a>{' '}
-        |
-        <a
-          href="https://codeadrian.github.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Personal website
-        </a>
-      </p>
     </div>
   </header>
 );
